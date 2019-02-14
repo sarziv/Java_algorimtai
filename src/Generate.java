c6c163e1966cad6a1b9bf1fb59392c45683e5f04
Sarunas Zivila <saruzivila12@gmail.com>
2019-02-14 23:51:19 +0200

    init

---
+++ src/Generate.java
@@ -0,0 +1,10 @@
+//generate data double(0.1234) and text(4)
+// example 0.1234 fgah
+public class Generate extends Main{
+
+    public static double randomGen() {
+    double data = Math.random();
+            return data;
+    }
+
+}
