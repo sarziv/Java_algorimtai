111fc887c912322666b0ee4525b36016bdbc136b
sarziv <saruzivila12@gmail.com>
2019-02-15 19:00:11 +0200

    init lb1

--- lab1/src/MergeInMemoryArray.java
+++ lab1/src/MergeInMemoryArray.java
@@ -1,6 +1,5 @@
 public class MergeInMemoryArray {
     public static void main(String[] args) {
-
-        System.out.println("Data"+ Genetare.randomGen);
+        system.out.println("Data");
     }
 }
